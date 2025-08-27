6870fc414df4c0e0a6cd6951451bf73808d33431
SheikhMdZiauddin <ziauddinjim@outlook.com>
2025-08-27 08:12:41 +0600

    call function create

--- script/index.js
+++ script/index.js
@@ -7,3 +7,29 @@ for(let button of heartButtons){
         document.getElementById('heart-count').textContent =counter;
     });
 }
+// Function to get id
+const getId= (id)=>{
+    const getId = document.getElementById(id).textContent;
+    return getId.trim();
+}
+
+// Call Feature
+// Coin id ="coin-count"
+// call id="call-btn"
+// id="service-name"
+// id="service-number"
+document.getElementById('call-btn').addEventListener('click', ()=>{
+    const coin =parseInt(getId('coin-count'));
+    const serviceName =getId("service-name");
+    const serviceNumber =getId("service-number");
+    const digit = 20;
+    const coinElement =document.getElementById('coin-count');
+    if(coin >= digit){
+        alert(`Calling ${serviceName} ${serviceNumber}...`);
+        const min = coin - digit;
+        coinElement.textContent= min;
+    }else{
+        alert(`You don't have enough coins, you need at least 20 coins to make a call.`);
+    }
+    
+})
